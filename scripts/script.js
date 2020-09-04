40a10b09f4767449a61df9d537bbb0491f4695cc
Renata R <renatarahmanina@yandex.ru>
2020-09-04 15:46:52 +0300

    fix: исправление ошибок после первой итерации

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,33 @@
 const popup = document.querySelector('.popup');
-const popupEditButton = document.querySelector('.js-profile__edit-button');
+const popupEditButton = document.querySelector('.profile__edit-button');
 const popupCloseButton = popup.querySelector('.popup__close');
 
 const formElement = document.querySelector('.popup__form');
-const nameInput = formElement.querySelector('.popup__input-item-name');
-const jobInput = formElement.querySelector('.popup__input-item-about');
+const nameInput = formElement.querySelector('.popup__input-item_name');
+const jobInput = formElement.querySelector('.popup__input-item_about');
 const profileName = document.querySelector('.profile__name');
 const profileSubline = document.querySelector('.profile__subline');
-
-
-
-const popupOpen = (event) => {
-  console.log('Event: ', event)
-  popup.classList.add('popup_opened')
-}
-
-const popupClose = (event) => {
-  console.log('Event: ', event)
-  popup.classList.remove('popup_opened')
-}
-
-popupEditButton.addEventListener('click', popupOpen);
-popupCloseButton.addEventListener('click', popupClose);
-
-
 const formSubmitButton = formElement.querySelector('.popup__form-submit-button');
+
+
+const popupOpen = () => {
+  popup.classList.add('popup_opened');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileSubline.textContent;
+}
+
+const popupClose = () => {
+  popup.classList.remove('popup_opened');
+}
 
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
     
     profileName.textContent = nameInput.value;
     profileSubline.textContent = jobInput.value;
-}
 
+    popupClose();
+}
+popupEditButton.addEventListener('click', popupOpen);
+popupCloseButton.addEventListener('click', popupClose);
 formElement.addEventListener('submit', formSubmitHandler);
-formSubmitButton.addEventListener('click', popupClose);
